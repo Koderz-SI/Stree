5f9ebc283a68f1f84564aa7850cd79d8a28a2ced
SanskritiKushik5 <sanskritikushik23@gmail.com>
2022-03-01 21:39:57 +0530

    home page

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
-const cookieParser = require('cookie-parser');
-const expressSession = require('express-session');
-var MemoryStore = require('memorystore')(expressSession);
-const passport = require('passport');
-const flash = require('connect-flash');
+const cookieParser = require("cookie-parser");
+const expressSession = require("express-session");
+var MemoryStore = require("memorystore")(expressSession);
+const passport = require("passport");
+const flash = require("connect-flash");
 
 const app = express();
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
-app.use('/assets', express.static(__dirname + '/assets'));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.use("/assets", express.static(__dirname + "/assets"));
 app.use(express.urlencoded({ extended: true }));
 
-const mongoURI = require('./config/mongoKey');
+const mongoURI = require("./config/mongoKey");
 mongoose
   .connect(mongoURI, {
     useNewUrlParser: true,
@@ -22,13 +22,13 @@ mongoose
     useFindAndModify: false,
     useCreateIndex: true,
   })
-  .then(() => console.log('Connected !'));
+  .then(() => console.log("Connected !"));
 
-app.use(cookieParser('random'));
+app.use(cookieParser("random"));
 
 app.use(
   expressSession({
-    secret: 'random',
+    secret: "random",
     resave: true,
     saveUninitialized: true,
     // setting the max age to longer duration
@@ -43,14 +43,14 @@ app.use(passport.session());
 app.use(flash());
 
 app.use(function (req, res, next) {
-  res.locals.success_messages = req.flash('success_messages');
-  res.locals.error_messages = req.flash('error_messages');
-  res.locals.error = req.flash('error');
+  res.locals.success_messages = req.flash("success_messages");
+  res.locals.error_messages = req.flash("error_messages");
+  res.locals.error = req.flash("error");
   next();
 });
 
-app.use(require('./controller/routes.js'));
+app.use(require("./controller/routes.js"));
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => console.log('Server Started At ' + PORT));
+app.listen(PORT, () => console.log("Server Started At " + PORT));
